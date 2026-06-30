ab50590fc9ea209a47b34bcd43f5022077bf1620
YellowRiver6 <2043332969@qq.com>
2026-06-30 15:31:16 +0800

    fix due to uws.js

--- src/colyseus.app.ts
+++ src/colyseus.app.ts
@@ -1,4 +1,4 @@
-import {defineServer, defineRoom, matchMaker, WebSocketTransport} from "colyseus";
+import {defineServer, defineRoom, matchMaker} from "colyseus";
 
 //import { monitor } from "@colyseus/monitor";
 //import { playground } from "@colyseus/playground";
@@ -8,7 +8,7 @@ import { GameRoom } from "./rooms/GameRoom";
 import { NetworkRoom } from "./rooms/NetworkRoom";
 import { setCooldown } from "./cooldown";
 // import { BunWebSockets } from "@colyseus/bun-websockets";
-//import  {uWebSocketsTransport} from "@colyseus/uwebsockets-transport";
+import  {uWebSocketsTransport} from "@colyseus/uwebsockets-transport";
 
 function registerRooms() {
     const rooms = {
@@ -39,14 +39,10 @@ export default defineServer({
     //         maxPayloadLength: 250 * 1024,
     //     }
     // }),
-    transport: new WebSocketTransport({
-        maxPayload: 1024 * 1024 * 250,
-    })
-    // transport: new uWebSocketsTransport({
-    //     maxPayloadLength: 20 * 1024,
-    //
-    // }, {
-    //     cert_file_name: process.env.NETWORK_CERT_FILE_NAME,
-    //     key_file_name: process.env.NETWORK_KEY_FILE_NAME,
+    // transport: new WebSocketTransport({
+    //     maxPayload: 1024 * 1024 * 250,
     // })
+    transport: new uWebSocketsTransport({
+        maxPayloadLength: 20 * 1024,
+    })
 });
